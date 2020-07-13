875f67bbccdec29b8ded9ee9cee7df2e55343bc6
jerrymmmrj <67281133+jerrymmmrj@users.noreply.github.com>
2020-07-13 22:42:46 +0800

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# This is the notebook for capstone project
+import numpy as np 
+import pandas as pd
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
